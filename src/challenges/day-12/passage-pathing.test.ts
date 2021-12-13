69cf6919a6bdcc3433a65cfc93d96eb8d909e413
Simon Cragg <simoncragg@icloud.com>
2021-12-13 20:22:46 +0000

    feat: day12

--- src/challenges/day-12/passage-pathing.test.ts
+++ src/challenges/day-12/passage-pathing.test.ts
@@ -1,4 +1,7 @@
-import { computeNumOfPathsThatVisitSmallCavesAtMostOnce } from "./passage-pathing";
+import {
+  computeNumOfPathsThatVisitSmallCavesAtMostOnce,
+  computeNumOfPathsThatVisitOneSmallCaveTwiceAndTheOtherSmallCavesJustOnce,
+} from "./passage-pathing";
 import { readFile } from "../../utils/file-util";
 
 describe("passage pathing", () => {
@@ -15,6 +18,25 @@ describe("passage pathing", () => {
 
       expect(
         computeNumOfPathsThatVisitSmallCavesAtMostOnce(caveConnections)
+      ).toEqual(expected);
+    });
+  });
+
+  describe("computeNumOfPathsThatVisitSmallCavesAtMostOnce", () => {
+    it.each([
+      ["example1.txt", 36],
+      ["example2.txt", 103],
+      ["example3.txt", 3509],
+      ["challenge.txt", 84870],
+    ])("when passed %p, returns %p", (filename: string, expected: number) => {
+      const caveConnections = readFile(`input/day-12/${filename}`)
+        .split("\n")
+        .map((s) => s.trim());
+
+      expect(
+        computeNumOfPathsThatVisitOneSmallCaveTwiceAndTheOtherSmallCavesJustOnce(
+          caveConnections
+        )
       ).toEqual(expected);
     });
   });
--- src/challenges/day-12/passage-pathing.ts
+++ src/challenges/day-12/passage-pathing.ts
@@ -1,63 +1,50 @@
+import { arraysEqual } from "../../utils/array-utils";
+
 export const computeNumOfPathsThatVisitSmallCavesAtMostOnce = (
   caveConnections: Array<string>
 ): number => {
-  const startCave = buildCaveTree(caveConnections);
-  const paths = buildPathsRecursive(startCave, [] as Path);
-  //paths.forEach((path) => console.log(path.join(",")));
+  const connections = buildConnections(caveConnections);
+  const startCave = buildCaveTree(connections);
+  const predicate = (fromCave: Cave, toCave: Cave, path: Path) =>
+    !isStart(toCave) &&
+    !isSmallAndAlreadyVisited(toCave, path) &&
+    !hasAlreadyTraversed(fromCave, toCave, path);
+
+  const paths = buildPathsRecursive(startCave, [] as Path, predicate);
+  //logPaths(paths.map((path) => path.join(",")));
   return paths.length;
 };
 
-function buildPathsRecursive(cave: Cave, path: Path): Array<Path> {
-  const paths = Array<Path>();
-  path.push(cave.id);
+export const computeNumOfPathsThatVisitOneSmallCaveTwiceAndTheOtherSmallCavesJustOnce =
+  (caveConnections: Array<string>): number => {
+    const connections = buildConnections(caveConnections);
+    const startCave = buildCaveTree(connections);
+    const smallCaves = getDistinctSmallCaves(connections);
 
-  if (cave.id === "end") {
-    paths.push(path);
-    return paths;
-  }
+    let allPaths = Array<Path>();
+    for (const specialCaseId of smallCaves) {
+      const predicate = (fromCave: Cave, toCave: Cave, path: Path) =>
+        !isStart(toCave) &&
+        !isOverVisitedSmallCave(toCave, path, specialCaseId);
 
-  const linkedCaves = cave.linkedCaves.filter(
-    (linkedCave) =>
-      !isStart(linkedCave) &&
-      !isSmallAndAlreadyVisited(linkedCave, path) &&
-      !hasAlreadyTraversed(cave, linkedCave, path)
-  );
-  if (linkedCaves.length > 0) {
-    for (const linkedCave of linkedCaves) {
-      const newPath = [...path];
-      const newPaths = buildPathsRecursive(linkedCave, newPath);
-      for (const newPath of newPaths) {
-        paths.push(newPath);
-      }
+      const paths = buildPathsRecursive(
+        startCave,
+        [] as Path,
+        predicate,
+        specialCaseId
+      );
+
+      allPaths = [...allPaths, ...paths];
     }
-  }
-  return paths;
-}
 
-function isStart(cave: Cave): boolean {
-  return cave.id === "start";
-}
+    // TODO: optimise
+    const distinctPaths = allPaths
+      .map((path) => path.join(","))
+      .filter((path, i, self) => self.indexOf(path) === i);
 
-function isSmallAndAlreadyVisited(cave: Cave, path: Array<string>): boolean {
-  return cave.isSmall && path.indexOf(cave.id) > -1;
-}
-
-function hasAlreadyTraversed(
-  cave: Cave,
-  linkedCave: Cave,
-  path: Array<string>
-): boolean {
-  return path.join(",").indexOf(`${cave.id},${linkedCave.id}`) > -1;
-}
-
-function buildCaveTree(caveConnections: Array<string>): Cave {
-  const connections = buildConnections(caveConnections);
-  const caves = Array<Cave>();
-  for (const connection of connections) {
-    processConnection(connection, caves);
-  }
-  return caves.find((cave) => cave.id === "start")!;
-}
+    //logPaths(distinctPaths);
+    return distinctPaths.length;
+  };
 
 function buildConnections(caveConnections: Array<string>): Array<Connection> {
   return caveConnections.map((cc) => {
@@ -67,6 +54,14 @@ function buildConnections(caveConnections: Array<string>): Array<Connection> {
       to: caves[1],
     };
   });
+}
+
+function buildCaveTree(connections: Array<Connection>): Cave {
+  const caves = Array<Cave>();
+  for (const connection of connections) {
+    processConnection(connection, caves);
+  }
+  return caves.find((cave) => cave.id === "start")!;
 }
 
 function processConnection(connection: Connection, caves: Array<Cave>) {
@@ -93,4 +88,108 @@ function processConnection(connection: Connection, caves: Array<Cave>) {
 
   caveA.linkedCaves.push(caveB);
   caveB.linkedCaves.push(caveA);
+}
+
+function buildPathsRecursive(
+  cave: Cave,
+  path: Path,
+  predicate: (fromCave: Cave, toCave: Cave, path: Path) => boolean,
+  specialCaseId: string | undefined = undefined
+): Array<Path> {
+  const paths = Array<Path>();
+  path.push(cave.id);
+
+  if (cave.id === "end") {
+    paths.push(path);
+    return paths;
+  }
+
+  const linkedCaves = cave.linkedCaves.filter((linkedCave) =>
+    predicate(cave, linkedCave, path)
+  );
+  if (linkedCaves.length > 0) {
+    for (const linkedCave of linkedCaves) {
+      const newPath = [...path];
+      const newPaths = buildPathsRecursive(
+        linkedCave,
+        newPath,
+        predicate,
+        specialCaseId
+      );
+      for (const newPath of newPaths) {
+        paths.push(newPath);
+      }
+    }
+  }
+  return paths;
+}
+
+function isStart(cave: Cave): boolean {
+  return cave.id === "start";
+}
+
+function isSmallAndAlreadyVisited(cave: Cave, path: Array<string>): boolean {
+  return cave.isSmall && path.indexOf(cave.id) > -1;
+}
+
+function hasAlreadyTraversed(
+  fromCave: Cave,
+  toCave: Cave,
+  path: Path,
+  specialCaseId: string | undefined = undefined
+): boolean {
+  return toCave.id === specialCaseId
+    ? path.filter((id) => id === specialCaseId).length >= 2
+    : path.join(",").indexOf(`${fromCave.id},${toCave.id}`) > -1;
+}
+
+function isOverVisitedSmallCave(
+  cave: Cave,
+  path: Path,
+  specialCaseId: string
+): boolean {
+  const maxVisits = cave.id === specialCaseId ? 2 : 1;
+  return (
+    cave.isSmall && path.filter((id) => id === cave.id).length >= maxVisits
+  );
+}
+
+function getUniquePaths(newPaths: Array<Path>, existingPaths: Array<Path>) {
+  if (existingPaths.length === 0) {
+    return newPaths;
+  }
+
+  let uniquePaths = [];
+  for (const newPath of newPaths) {
+    let isUnique = true;
+    for (const path of existingPaths) {
+      if (arraysEqual(newPath, path)) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) {
+      uniquePaths.push(newPath);
+    }
+  }
+
+  return uniquePaths;
+}
+
+function getDistinctSmallCaves(connections: Array<Connection>) {
+  const tos = connections
+    .filter((c) => c.to === c.to.toLowerCase())
+    .map((c) => c.to);
+  const froms = connections
+    .filter((c) => c.from === c.from.toLowerCase())
+    .map((c) => c.from);
+  return [...tos, ...froms]
+    .filter((id) => id !== "start" && id !== "end")
+    .filter((id, i, self) => self.indexOf(id) === i);
+}
+
+function logPaths(paths: Array<string>) {
+  let pathsLog = "";
+  paths.forEach((path) => (pathsLog += `${path}\n`));
+  console.log(pathsLog);
 }
